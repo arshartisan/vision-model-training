7c2b1b99cc65344f7f4e336cabfcd5518c45dd08
arshartisan <arshartisan@gmail.com>
2026-01-01 14:47:25 +0530

    feat: add ROIModule and BatchModule to DetectionModule imports

--- salt-detection-backend/src/detection/detection.module.ts
+++ salt-detection-backend/src/detection/detection.module.ts
@@ -3,9 +3,11 @@ import { DetectionController } from './detection.controller';
 import { DetectionService } from './detection.service';
 import { DetectionGateway } from './detection.gateway';
 import { InferenceModule } from '../inference/inference.module';
+import { ROIModule } from '../roi/roi.module';
+import { BatchModule } from '../batch/batch.module';
 
 @Module({
-  imports: [InferenceModule],
+  imports: [InferenceModule, ROIModule, BatchModule],
   controllers: [DetectionController],
   providers: [DetectionService, DetectionGateway],
   exports: [DetectionService],
