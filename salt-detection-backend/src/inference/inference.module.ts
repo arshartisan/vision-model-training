03a303bfc39fe913d4f78034b7417217de9214f9
arshartisan <arshartisan@gmail.com>
2026-01-01 20:02:57 +0530

    feat: add WhitenessService to InferenceModule providers and exports

--- salt-detection-backend/src/inference/inference.module.ts
+++ salt-detection-backend/src/inference/inference.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { InferenceService } from './inference.service';
 import { PreprocessingService } from './preprocessing.service';
 import { PostprocessingService } from './postprocessing.service';
+import { WhitenessService } from './whiteness.service';
 
 @Module({
-  providers: [InferenceService, PreprocessingService, PostprocessingService],
-  exports: [InferenceService],
+  providers: [InferenceService, PreprocessingService, PostprocessingService, WhitenessService],
+  exports: [InferenceService, WhitenessService],
 })
 export class InferenceModule {}
